dff0d6ee893e0ba344539c20d04107fe085e1cd2
asimchauhan987-byte <asimchauhan987@gmail.com>
2026-03-20 21:51:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("Website working");
+function openMenu(){
+
+let m = document.getElementById("menu");
+
+if(m.style.width=="250px"){
+m.style.width="0";
+}else{
+m.style.width="250px";
+}
+
+}
